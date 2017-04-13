18716b224ea5bc1e178d80d41179815a701186d8
MartinHengsberger <martin.hengsberger@gmail.com>
2017-04-13 15:30:46 +0200

    Layout Collection Test

--- CardGame/CardGame.Web/Controllers/UserController.cs
+++ CardGame/CardGame.Web/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using CardGame.DAL.Logic;
 using CardGame.DAL.Model;
 using CardGame.Log;
 using CardGame.Web.Models;
+using System.Net;
 
 namespace CardGame.Web.Controllers
 {
@@ -53,6 +54,65 @@ namespace CardGame.Web.Controllers
 
 
             return View(user);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "user")]
+        public ActionResult Profile(User user)
+        {
+           
+                if (user.Password != null)
+                {
+                    //Passwort ändern
+
+                    TempData["pchange"] = "Password changed!";
+                    return RedirectToAction("Profile", user.ID);
+                }
+
+                if (user.Email != null)
+                {
+                    //Email ändern
+
+                    TempData["echange"] = "Email changed!";
+                    return RedirectToAction("Profile", user.ID);
+                }
+            else
+            {
+                return RedirectToAction("Profile", user.ID);
+            }
+
+        }
+
+        public ActionResult _Cardcollection()
+        {
+            List<CardCollection> CollectionCardList = new List<CardCollection>();
+            var dbUserCardList = DeckManager.GetAllCollectionCards(UserManager.GetUserByUserEmail(User.Identity.Name).idperson);
+
+            foreach (var c in dbUserCardList)
+            {
+                //Wenn kein Index vorhanden ist -> index = -1
+                int index = CollectionCardList.FindIndex(i => i.IdCard == c.idcard);
+
+                if (index >= 0)
+                { CollectionCardList[index].Number += 1; }
+                else
+                {
+                    CardCollection card = new CardCollection();
+                    card.IdCard = c.idcard;
+                    card.IdUser = c.fkperson;
+                    card.IdCollectioncard = c.idcollectioncard;
+                    card.IdOrder = c.fkorder;
+                    card.Number = 1;
+                    card.Cardname = c.cardname;
+                    card.Attack = c.attack;
+                    card.Mana = c.mana;
+                    card.Life = c.life;
+                    card.pic = c.pic;
+
+                    CollectionCardList.Add(card);
+                }
+            }
+            return PartialView(CollectionCardList);
         }
 
     }
